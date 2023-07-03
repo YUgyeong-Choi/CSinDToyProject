65b0edd3e25e80f4a51896993f3dbcb4e739e762
최유경 <69104743+YUkyeong1@users.noreply.github.com>
2023-07-03 13:56:31 +0900

    초기 세팅

---
+++ Source/CsinDProject/CsinDProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CsinDProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CsinDProject, "CsinDProject" );
---
+++ Source/CsinDProject/CsinDProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CsinDProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CSINDPROJECT_API ACsinDProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
