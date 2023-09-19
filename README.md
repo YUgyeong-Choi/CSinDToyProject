4f5ec737b79f0f3d164f85437eab5c9c8bb39021
Yugeyong <69104743+YUgyeong-Choi@users.noreply.github.com>
2023-09-19 20:17:53 +0900

    프로젝트 재생성

--- README.md
+++
@@ -1,2 +0,0 @@
-# CSinDToyProject
-게임 개발 동아리 CSinD에서 5명 동아리 원들과 함께 진행하는 토이 프로젝트
--- Source/CsinDProject/CsinDProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CsinDProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CsinDProject, "CsinDProject" );
